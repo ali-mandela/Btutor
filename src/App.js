f2a23880ca9fe62f2af69963b894a88c0a47cd1f
ali mandela <muhammadalimandela01@gmail.com>
2022-09-27 18:20:43 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Route ,Routes} from 'react-router-dom';
 import './App.css';
-import Ad from './components/ad';
+import About from './components/About';
+import Apply from './components/Apply';
+import Career from './components/Career';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Policies from './components/Policies';
 function App() {
   return (<>
-  <Ad/> 
+  <Navbar/>
+  <Routes> 
+  <Route exact path='/' element={<Home/>}/>
+  <Route path='/about' element={<About/>}/>
+  <Route path='/career' element={<Career/>}/>
+  {/* <Route path='/policies' element={<Policies/>}/> */}
+  <Route path='/apply' element={<Apply/>}/>
+  <Route path='/contact' element={<Contact/>}/>
+  </Routes>
   </>);
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../Assets/logo.png';
+const Navbar = () => {
+  return (<>
+    <section className='navbar-main'>
+        <div className='navbar'>
+            <ul>
+                {/* <li><NavLink to='/'>Home</NavLink></li> */}
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to='/contact'>Contact</NavLink></li>
+                <li><NavLink><img className='imglogo' src={logo} alt="logo"/></NavLink></li>
+                {/* <li><NavLink to='/policies'>Policies</NavLink></li> */}
+                <li><NavLink to='/career' >Career</NavLink></li>
+                <li><NavLink to='/apply'>Apply now</NavLink></li>
+            </ul>
+        </div>
+    </section>
+  </>)
+}
+
+export default Navbar
